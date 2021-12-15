182e6cefe75d63211e1b95fbd25cabfb628ea3fd
Ngala Talla <ngala.talla@mdh.se>
2021-12-15 16:26:40 +0100

    Ny data structure

---
+++ ConsoleApp3/Trie.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp3
+{
+  public class Trie
+  {
+    public class TrieNode
+    {
+      private bool end = false;
+      private static int MAX = 26;
+      private int[] Children = new int[MAX];
+    }
+
+    public void Insert(string word)
+    {
+
+    }
+
+    public void Remove(string word)
+    {
+
+    }
+    public void Print()
+    {
+
+    }
+
+    public void AutoComplete()
+    {
+
+    }
+  }
+}
